6a7893da91df995a1484883c7133e8f71643cf0e
Prem <prem7736k@gmail.com>
2025-08-07 10:15:54 +0530

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Timeline for section .two
-var tl = gsap.timeline({
+const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".two",
     start: "0% 95%",
@@ -11,77 +11,89 @@ var tl = gsap.timeline({
 
 tl.to("#fanta", {
   top: "120%",
-  left: "0%"
+  left: "0%",
+  ease: "power2.out"
 }, 'orange');
 
 tl.to("#orange-cut", {
   top: "160%",
-  left: "23%"
+  left: "23%",
+  ease: "power2.out"
 }, 'orange');
 
 tl.to("#orange", {
   width: "15%",
   top: "160%",
-  right: "10%"
+  right: "10%",
+  ease: "power2.out"
 }, 'orange');
 
 tl.to("#leaf", {
   top: "110%",
   left: "70%",
-  rotate: 130
+  rotate: 130,
+  ease: "power2.out"
 }, 'orange');
 
 tl.to("#leaf2", {
   top: "110%",
   left: "0%",
-  rotate: 130
+  rotate: 130,
+  ease: "power2.out"
 }, 'orange');
 
 
 // Timeline for section .three
-var tl2 = gsap.timeline({
+const tl2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".three",
     start: "0% 95%",
-    end: "20% 50%",
+    end: "30% 50%", // Increased range for better visibility
     scrub: true,
     // markers: true,
   }
 });
 
+// Entry animations from left and right
 tl2.from(".lemon1", {
   rotate: -90,
   left: "-100%",
-  top: "110%"
+  top: "110%",
+  ease: "power2.out"
 }, 'ca');
 
 tl2.from("#cocacola", {
   rotate: -90,
   top: "110%",
-  left: "-100%"
+  left: "-100%",
+  ease: "power2.out"
 }, 'ca');
 
 tl2.from(".lemon2", {
   rotate: 90,
   left: "100%",
-  top: "110%"
+  top: "110%",
+  ease: "power2.out"
 }, 'ca');
 
 tl2.from("#pepsi", {
   rotate: 90,
   top: "110%",
-  left: "100%"
+  left: "100%",
+  ease: "power2.out"
 }, 'ca');
 
-// Animate reused assets into center position
+// Animate reused Fanta elements into center view
 tl2.to("#orange-cut", {
   width: "18%",
   left: "42%",
-  top: "204%"
-}, 'ca');
+  top: "204%",
+  ease: "power2.out"
+}, 'ca+=0.2'); // delay slightly after entrance
 
 tl2.to("#fanta", {
   width: "35%",
   top: "210%",
-  left: "33%"
-}, 'ca');
+  left: "33%",
+  ease: "power2.out"
+}, 'ca+=0.2');
